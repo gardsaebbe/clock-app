c934534ddc33545d96febf0ddd0e14438b3bb77d
Gard.Sabbe <gasae8567@uib.no>
2025-10-13 12:46:58 +0200

    done with start and reset

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,77 @@
-import { Stack, Link } from 'expo-router';
+import { Stack} from 'expo-router';
 
-import { View } from 'react-native';
-
-import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
+import {Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+  const[time, setTime] = useState(0);
+  const[counting, setCounting] = useState(false); 
+
+  const startTimeRef = useRef<number | null>(null);
+  const intervalRef = useRef<number | null>(null);
+  const savedTimeRef = useRef<number>(0);
+
+
+  useEffect(() => {
+    if (counting){
+      startTimeRef.current = Date.now() - savedTimeRef.current;
+
+      intervalRef.current = setInterval(() => {
+      
+        if (startTimeRef.current){
+          const elapsed = Date.now() - startTimeRef.current;
+          setTime(elapsed);
+        }
+      }, 50)
+    } else{
+      savedTimeRef.current = time;
+      if (intervalRef.current){
+        clearInterval(intervalRef.current);
+      }
+    }
+    return () => {
+      if (intervalRef.current){
+        clearInterval(intervalRef.current);
+      }
+    }
+
+  }, [counting])
+
+  const toggleCounting = () => setCounting (!counting);
+
+  const handleReset = () => {
+    setTime(0);
+    setCounting(false);
+    savedTimeRef.current = 0;
+  }
+
+  const formatTime= () => {
+    const ms = ((Math.floor(time / 1000))/10);
+    const sec = ((Math.floor(time / 1000))%60);
+    const min = ((Math.floor(time / 60000))%60);
+    return `${String(min).padStart(2,'0')}:${String(sec).padStart(2,'0')}:${String(ms).padStart(2,'0')}`
+  }
   return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
+    <>
+      <Stack.Screen options={{ title: 'StopWatch' }} />
       <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
+        <View className = "w-full mt-12">
+          <Text className = "text-center font-mono text-7xl">{formatTime()}</Text>
+          </View>
+
+          <View className='flex flex-row justify-between'>
+          <TouchableOpacity onPress={handleReset} className='w-20 h-20 item-center rounded-full bg-gray-500 justify-center'>
+            <Text className='text-xl text-white'>Reset</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={toggleCounting} className='w-20 h-20 item-center rounded-full bg-green-800 justify-center'>
+            <Text className='text-xl text-white'>Start</Text>
+          </TouchableOpacity>
+          </View>
+
+          <View className='my-8 h-[1px] w-full bg-gray-300'> </View>
       </Container>
-    </View>
+    </>
   );
 }
 
